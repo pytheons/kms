445706a3c805b55a7627694635b4cb1c40ff1e60
Michael Ligus <maveilthain@gmail.com>
2025-04-11 23:02:12 +0200

    [004] feat(add): add entry command

--- kms/application/commands.py
+++ kms/application/commands.py
@@ -1,27 +1,61 @@
+import os.path
 from functools import singledispatchmethod
 from typing import (
     Any,
 )
 
 from cleo.commands.command import Command
-from cleo.helpers import option
+from cleo.helpers import (
+    argument,
+    option,
+)
 
-from kms.application.configuration import KMSConfiguration
+from kms.application.configuration import Configuration
 from kms.application.queries import (
     AbstractQuery,
     BooleanQuery,
     ChoseCredentialsQuery,
+    DatabaseQuery,
     OptionQuery,
     PasswordQuery,
+    SecretQuery,
     ValueQuery,
 )
 from kms.infrastructure.adapters import KeePassXC
 
 
-class KmsCommand(Command):
-    configuration = KMSConfiguration
+class BaseCommand(Command):
+    _default_options = []
 
-    def __init__(self, configuration: KMSConfiguration) -> None:
+    def __new__(cls, *args, **kwargs):
+        cls.options.extend(cls._default_options)
+        return super().__new__(cls)
+
+    def question(self, text: str) -> None:
+        self.write(text, "question")
+
+
+class KmsCommand(BaseCommand):
+    configuration = Configuration
+    _default_options = [
+        option(
+            "database",
+            "d",
+            "Specify the path to the KeePass database when initializing, accessing or modifying the database. The config is ignored when this is given.",
+        ),
+        option(
+            "keyfile",
+            "k",
+            "Specify the path to the keyfile when initializing, accessing or modifying the database.",
+        ),
+        option(
+            "config",
+            "c",
+            "Specify the path to the config file when initializing, accessing or modifying the database.",
+        ),
+    ]
+
+    def __init__(self, configuration: Configuration) -> None:
         super().__init__()
         self.configuration = configuration
 
@@ -29,17 +63,36 @@ class KmsCommand(Command):
     def prompt(self, query: AbstractQuery) -> Any:
         raise NotImplementedError
 
-    @staticmethod
-    def escape(result: str) -> str:
-        return result and str(result).strip("\n").strip() or None
-
     @prompt.register
     def __prompt_for_option(self, query: OptionQuery) -> str:
         option_value = self.option(query.option)
-        if not option_value:
+        if not option_value and query.init:
             option_value = self.prompt(ValueQuery(question=query.question, default=query.default))
 
-        return self.escape(option_value)
+        return self.escape(option_value) or query.default
+
+    @prompt.register
+    def __prompt_for_database(self, query: DatabaseQuery) -> str:
+        return self.prompt(
+            OptionQuery(
+                option=query.option,
+                question=query.question,
+                default=query.default,
+                init=query.init,
+            )
+        )
+
+    @property
+    def database(self) -> str:
+        return self.configuration.database.path
+
+    @property
+    def keyfile(self) -> str:
+        return self.configuration.keyfile.path
+
+    @staticmethod
+    def is_not_exists(path: str):
+        return not os.path.exists(path)
 
     @prompt.register
     def __prompt_for_boolean(self, query: BooleanQuery) -> bool | Any:
@@ -49,16 +102,18 @@ class KmsCommand(Command):
 
     @prompt.register
     def __prompt_for_value(self, query: ValueQuery) -> bool | Any:
-        self.io.write(query.question)
-
-        return self.escape(self.io.read_line(default=query.default)) or query.default
+        return self.ask(query.question) or query.default
 
     @prompt.register
     def __prompt_for_credentials(self, query: ChoseCredentialsQuery) -> bool | Any:
         password = None
         keyfile = None
+        is_password = False
 
-        match self.prompt(BooleanQuery(question=query.question, default=query.default)):
+        if query.init:
+            is_password = self.prompt(BooleanQuery(question=query.question, default=query.default))
+
+        match is_password:
             case True:
                 password = self.prompt(PasswordQuery(question="Password:", default=None))
             case False:
@@ -66,7 +121,8 @@ class KmsCommand(Command):
                     OptionQuery(
                         "keyfile",
                         question="Desired keyfile path: ",
-                        default=str(self.configuration["keyfile"]["path"]).format("kms"),
+                        default=("{}" in self.keyfile and self.keyfile.format("kms")) or self.keyfile,
+                        init=query.init,
                     )
                 )
 
@@ -78,8 +134,50 @@ class KmsCommand(Command):
         confirm = self.secret(question=f"Confirm {query.question.lower()}", default=query.default)
         if password != confirm:
             self.line_error("ERROR: Passwords do not match", style="error")
+            return False
 
         return password
+
+    @prompt.register
+    def __prompt_for_secret(self, query: SecretQuery) -> bool | Any:
+        return self.secret(question=query.question, default=None)
+
+    @staticmethod
+    def escape(result: str) -> str:
+        return result and str(result).strip("\n\n").strip() or None
+
+    def open(self, name: str = "kms") -> KeePassXC:
+        database_path, password, keyfile = self.prompt_default_options(name)
+        return KeePassXC(
+            filename=database_path,
+            password=password,
+            keyfile=keyfile,
+            readonly=True,
+        )
+
+    def prompt_default_options(
+        self,
+        name: str = "kms",
+        init: bool = False,
+    ):
+        database_path = self.prompt(
+            DatabaseQuery(
+                "database",
+                question="Desired database path: ",
+                default=("{}" in self.database and self.database.format(name)) or self.database,
+                init=init,
+            )
+        )
+
+        password, keyfile = self.prompt(
+            ChoseCredentialsQuery(
+                question="Password protect database? [y/N]: ",
+                default=False,
+                init=init,
+            ),
+        )
+
+        return database_path, password, keyfile
 
 
 class Init(KmsCommand):
@@ -88,21 +186,7 @@ class Init(KmsCommand):
         option(
             "name",
             "N",
-            """
-             Specify the name of KeePass database when initializing.
-            """,
-        ),
-        option(
-            "database",
-            "d",
-            """Specify the path to the KeePass database when initializing, accessing or modifying the database.
-            The config is ignored when this is given.""",
-        ),
-        option(
-            "keyfile",
-            "k",
-            """Specify the path to the keyfile when initializing, accessing or modifying the database.
-            """,
+            "Specify the name of KeePass database when initializing.",
         ),
     ]
 
@@ -112,22 +196,12 @@ class Init(KmsCommand):
                 "name",
                 question="Database name (no spaces): ",
                 default="kms",
-            )
-        )
-        database_path = self.prompt(
-            OptionQuery(
-                "database",
-                question="Desired database path: ",
-                default=str(self.configuration["database"]["path"]).format(name),
+                init=True
             )
         )
 
-        password, keyfile = self.prompt(
-            ChoseCredentialsQuery(
-                question="Password protect database? [y/N]: ",
-                default=False,
-            ),
-        )
+        database_path, password, keyfile = self.prompt_default_options(name, init=True)
+
         if any([password, keyfile]):
             self.create(dict(database=database_path, password=password, keyfile=keyfile))
             return
@@ -138,21 +212,85 @@ class Init(KmsCommand):
         )
 
     def create(self, database_parameters: dict[str, str | int | bool]) -> None:
+        self.info("Creating database...")
         kee_pass_xc = KeePassXC(
             filename=database_parameters["database"],
             password=database_parameters["password"],
             keyfile=database_parameters["keyfile"],
         )
         if kee_pass_xc.keyfile:
-            kee_pass_xc.create_keyfile(content=self.configuration["keyfile"]["content"])
+            kee_pass_xc.create_keyfile(content=self.configuration.content())
 
         kee_pass_xc.create()
+        self.configuration.dump()
+
+        self.line("Database created. [ <comment>DONE</> ]")
 
 
 class Add(KmsCommand):
-    arguments = []
+    name = "add"
+    options = [
+        option(
+            "username",
+            "u",
+            """Username is used by password store in database entry.""",
+        ),
+        option(
+            "word",
+            "w",
+            """Words size for generate secret or password. Defaults to 5.""",
+        ),
+        option(
+            "alphanumeric",
+            "a",
+            """Length of alphanumeric characters. Defaults to 32.""",
+        ),
+        option(
+            "symbolic",
+            "s",
+            """Length of symbolic characters. Defaults to 32.""",
+        ),
+        option(
+            "append",
+            None,
+            "Use --append to append STR to the end of the generated secret to meet specific password requirements.",
+        ),
+        option(
+            "fields",
+            "f",
+            "Use --fields to specify a comma separated list of custom fields to prompt for during entry creation." "",
+        ),
+    ]
+    arguments = [
+        argument("PATH", "The full path to the entry or group"),
+    ]
 
-    def handle(self): ...
+    def handle(self):
+        kee_pass_xc = self.open()
+        # word = self.option("word")
+        # # alphanumeric = self.option("alphanumeric")
+        # # symbolic = self.option("symbolic")
+        # # append = self.option("append")
+        # # fields = self.option("fields")
+        group, key = self.extract_group_from_path()
+        #
+        if group:
+            kee_pass_xc.add_group(group, group)
+        #
+        if key:
+            value = self.prompt(SecretQuery(question="Secret: ", default=None))
+            username = self.option("username") or key
+            kee_pass_xc.add_entry([], key, username=username, password=value)
+
+    def extract_group_from_path(self):
+        path = self.argument("PATH")
+        group, *entry = path and path.strip("/").split("/")
+        if group and not entry:
+            entry = [group]
+            group = None
+
+        key = "/".join(entry)
+        return group, key
 
 
 class Show(KmsCommand):
@@ -186,6 +324,12 @@ class List(KmsCommand):
 
 
 class Grep(KmsCommand):
+    arguments = []
+
+    def handle(self): ...
+
+
+class UnsealVault(KmsCommand):
     arguments = []
 
     def handle(self): ...
